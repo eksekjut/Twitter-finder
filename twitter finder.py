6096f741ee6371108de516f8d4ca9e036589e3ab
eksekjut <81157619+eksekjut@users.noreply.github.com>
2022-12-10 00:44:37 +0100

    Update twitter finder.py
    Added Data base which sorts all accounts

--- twitter finder.py
+++ twitter finder.py
@@ -1,21 +1,88 @@
 # Import the necessary libraries
 import tweepy
-from datetime import datetime
+import sqlite3
 
-# Authenticate with Twitter using your API keys
+# Set the Twitter API credentials
+consumer_key = "..."
+consumer_secret = "..."
+access_key = "..."
+access_secret = "..."
+
+# Authenticate with the Twitter API using the credentials
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
 
-# Get a list of your followers
-followers = api.followers()
+# Prompt the user for the username of the account to analyze
+username = input("Enter the username of the account to analyze: ")
 
-# Filter out followers without profile pictures and whose accounts were created between 2012 and 2016
+# Get the user's followers
+followers = api.followers(username)
+
+# Loop through the followers and exclude those without profile pictures
+# and those with accounts created between 2012 and 2016
 filtered_followers = []
 for follower in followers:
-  if follower.profile_image_url and 2012 <= follower.created_at.year <= 2016:
+  if follower.profile_image_url and follower.created_at.year < 2012 or follower.created_at.year > 2016:
     filtered_followers.append(follower)
 
-# Print the remaining followers
+# Connect to a SQLite database
+conn = sqlite3.connect("followers.db")
+
+# Create a table in the database to store the filtered followers
+conn.execute("CREATE TABLE IF NOT EXISTS followers (username TEXT, created_at TEXT)")
+
+# Loop through the filtered followers and insert their usernames and creation dates
 for follower in filtered_followers:
-  print(follower.screen_name)
+  conn.execute("INSERT INTO followers (username, created_at) VALUES (?, ?)", (follower.username, follower.created_at))
+
+# Save the changes and close the connection to the database
+conn.commit()
+conn.close()
+
+# Print the number of followers remaining after filtering
+print(f"{len(filtered_followers)} followers remaining after filtering")
+# Import the necessary libraries
+import tweepy
+import sqlite3
+
+# Set the Twitter API credentials
+consumer_key = "..."
+consumer_secret = "..."
+access_key = "..."
+access_secret = "..."
+
+# Authenticate with the Twitter API using the credentials
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
+
+# Prompt the user for the username of the account to analyze
+username = input("Enter the username of the account to analyze: ")
+
+# Get the user's followers
+followers = api.followers(username)
+
+# Loop through the followers and exclude those without profile pictures
+# and those with accounts created between 2012 and 2016
+filtered_followers = []
+for follower in followers:
+  if follower.profile_image_url and follower.created_at.year < 2012 or follower.created_at.year > 2016:
+    filtered_followers.append(follower)
+
+# Connect to a SQLite database
+conn = sqlite3.connect("followers.db")
+
+# Create a table in the database to store the filtered followers
+conn.execute("CREATE TABLE IF NOT EXISTS followers (username TEXT, created_at TEXT)")
+
+# Loop through the filtered followers and insert their usernames and creation dates
+for follower in filtered_followers:
+  conn.execute("INSERT INTO followers (username, created_at) VALUES (?, ?)", (follower.username, follower.created_at))
+
+# Save the changes and close the connection to the database
+conn.commit()
+conn.close()
+
+# Print the number of followers remaining after filtering
+print(f"{len(filtered_followers)} followers remaining after filtering")
